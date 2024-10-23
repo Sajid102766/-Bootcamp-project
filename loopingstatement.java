39d079618fa24f53ff01f1c9a064093ed2d00a5d
Sajid102766 <alamsajid9264@gmail.com>
2024-10-23 15:36:44 +0530

    Add files via upload

---
+++ loopingstatement.java
@@ -0,0 +1,26 @@
+public class loopingstatement {
+    
+
+public static void main(String[] args)
+{
+int no = 5;
+System.out.println(" for loop start");
+for (int i =0;  true; i++)
+{
+    if(i==20)
+    {
+         break;
+        //  continue;
+    }
+    System.out.println("value of i="+i);
+    
+
+}
+    System.out.println(" for loop end");
+  System.out.println("While loop start");
+  int i = 0; 
+
+  while(i <no);
+    
+}
+}
